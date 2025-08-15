8f5a3a388465ce01e06e2b47aab079909d5a7df5
Nicholas Mabaso <peaknicholas05@gmail.com>
2025-08-15 16:26:45 +0200

    Create config.py

---
+++ simulator/config.py
@@ -0,0 +1,12 @@
+# Configuration for ransomware simulation
+
+# Folder to simulate ransomware on
+TARGET_DIR = "../samples/original_files"
+
+# File extension for simulated encryption
+ENCRYPT_EXT = ".enc"
+
+# Ransom note content
+RANSOM_NOTE = """🚨 Your files have been encrypted!
+To recover them, contact attacker@example.com
+Do not modify files or try to restore from backup."""
